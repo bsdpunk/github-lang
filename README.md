032fcd313bc4a42dec78201dfef8c2908ce54bd9
dusty <bsdpunk@gmail.com>
2016-08-28 09:41:37 -0500

    updates

--- README.md
+++ README.md
@@ -11,3 +11,22 @@ bash get_percentages.sh
 
 Will ask if you have a token, then ask for the user you want.
 
+
+
+```
+$ bash get_percentages.sh 
+Enter your github token, if you have one. Otherwise hit enter: 
+
+Enter which github user you would like to check percentages on: bsdpunk
+
+Total LoC: 364832
+AppleScript 696 0.190773%
+Go 6761 1.85318%
+JavaScript 8323 2.28132%
+Perl 22165 6.0754%
+PHP 20528 5.6267%
+Python 302957 83.0401%
+R 1805Shell 0.494748%
+Shell 1597Awk 0.437736%
+```
+
--- get_percentages.sh
+++ get_percentages.sh
@@ -21,10 +21,10 @@ http https://api.github.com/users/$GHUSER/repos$CMD |python -m json.tool | egrep
 cat list | sed '/true/,+1d' | grep '"name"' | awk -F: '{print $0}' | tr -d '{' | tr -d '}' | tr -d '"' |sort | grep -v '^$'  | sed 's/^.*name://gi' | tr -d ',' >> repos
 for i in $(cat repos); do echo '' >> lang && http https://api.github.com/repos/$GHUSER/$i/languages$CMD >> lang; done;
 awk -F, '{print $0}' lang | tr -d '{' | tr -d '}' | tr -d '"'| tr ' ' '\n' |sort |grep -v 'HTML'| grep -v 'CSS'| grep -v '^$' | tr -d ',' >> clean
-cat clean
-cat list
-cat lang
-cat repos
+#cat clean
+#cat list
+#cat lang
+#cat repos
 bash tottals.awk
 rm -rf list 
 rm -rf repos
