a25fe04f7ef82044cccf02386a0ef8d2994c4d6b
dusty <bsdpunk@gmail.com>
2016-08-28 09:31:11 -0500

    here we go

--- get_percentages.sh
+++ get_percentages.sh
@@ -18,7 +18,7 @@ read -r GHUSER
 #echo 'https://api.github.com/users/bsdpunk/repos'$CMD
 http https://api.github.com/users/$GHUSER/repos$CMD |python -m json.tool | egrep '"name"|"fork"' > list
 #sleep 1
-cat list | sed '/true/,+1d' | grep '"name"' | awk -F, '{print $0}' | tr -d '{' | tr -d '}' | tr -d '"' |sort | grep -v '^$' | grep -v '^name:' | tr -d '^*name:'| tr -d ',' >> repos
+cat list | sed '/true/,+1d' | grep '"name"' | awk -F: '{print $0}' | tr -d '{' | tr -d '}' | tr -d '"' |sort | grep -v '^$'  | sed 's/^.*name://gi' | tr -d ',' >> repos
 for i in $(cat repos); do echo '' >> lang && http https://api.github.com/repos/$GHUSER/$i/languages$CMD >> lang; done;
 awk -F, '{print $0}' lang | tr -d '{' | tr -d '}' | tr -d '"'| tr ' ' '\n' |sort |grep -v 'HTML'| grep -v 'CSS'| grep -v '^$' | tr -d ',' >> clean
 cat clean
