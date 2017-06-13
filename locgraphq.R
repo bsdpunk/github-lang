66cce7858a8255a0d780ce08ed2a019bdcee45df
bsdpunk <bsdpunk@gmail.com>
2017-06-13 13:50:43 -0500

    Purdied up the graph on perqplot

--- locgraphq.R
+++ locgraphq.R
@@ -1,6 +1,8 @@
 library(ggplot2)
+library(scales)
 locs <- read.csv('loc.csv', header=TRUE, sep=",")
 Languages <- locs$language
+q <- qplot(x =locs$language,y =locs$LoC, color=Languages, xlab = "Language", ylab = "Lines Of Code", main = commandArgs(TRUE)[1], geom = "boxplot") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_y_continuous(labels = comma)
 png("loc.png")
-qplot(x =locs$language,y =locs$LoC, color=Languages, xlab = "Language", ylab = "Lines Of Code", main = commandArgs(TRUE)[1], geom = "boxplot")
+q
 dev.off()
