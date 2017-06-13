87de015f5caec49e943877bac37baee223f3d5ba
bsdpunk <bsdpunk@gmail.com>
2017-06-13 13:18:03 -0500

    I\'m stupid, bug fix, got wrong stuff qplot_perce

--- locgraphq.R
+++ locgraphq.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 locs <- read.csv('loc.csv', header=TRUE, sep=",")
+Languages <- locs$language
 png("loc.png")
-qplot(x =locs$language,y =locs$LoC, color=locs$language, xlab = "Language", ylab = "Lines Of Code", main = commandArgs(TRUE)[1], geom = "boxplot")
+qplot(x =locs$language,y =locs$LoC, color=Languages, xlab = "Language", ylab = "Lines Of Code", main = commandArgs(TRUE)[1], geom = "boxplot")
 dev.off()
