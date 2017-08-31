f8e50847d9f270525625ce515f49f4f9665224e3
bsdpunk <bsdpunk@gmail.com>
2017-08-30 20:35:35 -0500

    updateCatList

--- get_percentages_qplot.sh
+++ get_percentages_qplot.sh
@@ -21,7 +21,7 @@ read -r GHUSER
 #echo 'https://api.github.com/users/bsdpunk/repos'$CMD
 http https://api.github.com/users/$GHUSER/repos$CMD |python -m json.tool | egrep '"name"|"fork"' > list
 #sleep 1
-tail -r list | gsed '/true/,+1d' | grep '"name"' | awk -F: '{print $0}' | tr -d '{' | tr -d '}' | tr -d '"' |sort | grep -v '^$'  | gsed 's/^.*name://gi' | tr -d ',' >> repos
+cat list | gsed '/true/,+1d' | grep '"name"' | awk -F: '{print $0}' | tr -d '{' | tr -d '}' | tr -d '"' |sort | grep -v '^$'  | gsed 's/^.*name://gi' | tr -d ',' >> repos
 for i in $(cat repos); do echo '' >> lang && http https://api.github.com/repos/$GHUSER/$i/languages$CMD >> lang; done;
 awk -F, '{print $0}' lang | tr -d '{' | tr -d '}' | tr -d '"'| tr ' ' '\n' |sort | grep -v '^$' | tr -d ',' >> preclean
 cat preclean |perl -pe "s/(\d+|\d)([A-Za-z])/\1\n\2/gi" | grep -v 'HTML'| grep -v 'CSS' | sort >> clean
